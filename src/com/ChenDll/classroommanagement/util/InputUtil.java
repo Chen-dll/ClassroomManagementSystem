13fc154c2cbe1e2538d58272272cfad03e16749f
Chen-dll <64064977+Chen-dll@users.noreply.github.com>
2025-01-26 00:41:33 +0800

    Initial commit

--- src/com/ChenDll/classroommanagement/util/InputUtil.java
+++ src/com/ChenDll/classroommanagement/util/InputUtil.java
@@ -15,19 +15,4 @@ public class InputUtil {
         return Integer.parseInt(scanner.nextLine());
     }
 
-//    public static String retryInput(String message, int maxAttempts, InputValidator validator) {
-//        for (int i = 0; i < maxAttempts; i++) {
-//            String input = InputUtil.getString(message);
-//            if (validator.validate(input)) {
-//                return input;
-//            }
-//            System.out.println("输入无效！您还有 " + (maxAttempts - i - 1) + " 次尝试。");
-//        }
-//        throw new RuntimeException("超过最大尝试次数！");
-//    }
-//
-//    @FunctionalInterface
-//    public interface InputValidator {
-//        boolean validate(String input);
-//    }
 }
